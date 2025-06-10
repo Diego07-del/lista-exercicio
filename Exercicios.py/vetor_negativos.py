7bf5bcb86b88320c159bf29f9c60598c7e7dff0e
Diego Henrique de Jesus <diegohenriquedj07@gmail.com>
2025-06-10 08:34:27 -0300

    Update vetor_negativos.py

--- Exercicios.py/vetor_negativos.py
+++ Exercicios.py/vetor_negativos.py
@@ -1,9 +1,13 @@
 matriz = [0]*4
+cont = 0
 for i in range(len(matriz)):
     matriz[i]= input(f"Digite valores para a matriz[{i}]: ")
     matriz[i] = int(matriz[i])  
 for i in range(len(matriz)):
     if matriz[i] >0:
         print(matriz[i])
+        
     else:
+        cont +=1
         print()
+print(f" A quantidade de valores negativos é {cont}")
