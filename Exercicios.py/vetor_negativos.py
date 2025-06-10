7fb06ec2a37ce587bad05a54f47519ae70517971
Diego Henrique de Jesus <diegohenriquedj07@gmail.com>
2025-06-10 08:36:00 -0300

    Update vetor_negativos.py

--- Exercicios.py/vetor_negativos.py
+++ Exercicios.py/vetor_negativos.py
@@ -1,11 +1,11 @@
-matriz = [0]*4
+vetor = [0]*4
 cont = 0
-for i in range(len(matriz)):
-    matriz[i]= input(f"Digite valores para a matriz[{i}]: ")
-    matriz[i] = int(matriz[i])  
-for i in range(len(matriz)):
-    if matriz[i] >0:
-        print(matriz[i])
+for i in range(len(vetor)):
+    vetor[i]= input(f"Digite valores para a lista[{i}]: ")
+    vetor[i] = int(vetor[i])  
+for i in range(len(vetor)):
+    if vetor[i] >0:
+        print(vetor[i])
         
     else:
         cont +=1
